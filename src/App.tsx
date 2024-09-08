715e048b65135875f33cae609ba9f7ad55d29682
Daniil Solovei <gotodeeps@gmail.com>
2024-09-08 14:06:55 +0700

    feat: add main

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TextAnalyzerForm from './components/TextAnalyzerForm';
+import FileUploader from './components/FileUploader';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>Text and File Analysis</h1>
+        <div className="content">
+          <TextAnalyzerForm />
+          <FileUploader />
+        </div>
+      </div>
   );
-}
+};
 
 export default App;
